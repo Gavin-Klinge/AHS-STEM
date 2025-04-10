c2e6de8df9feb9ca5a0e933c21b3400bac3a3424
Gavin Klinge <78894884+Gavin-Klinge@users.noreply.github.com>
2025-04-10 12:23:59 -0500

    Update playback_speed.py

--- Computer Science/Templates/CS50-Based/playback_speed.py
+++ Computer Science/Templates/CS50-Based/playback_speed.py
@@ -1,7 +1,36 @@
 '''
-Name: First 
+Name: Last, First
+Class Block: #suffix Block (*example* 3rd Block)
 Date of Creation: ##/##/####
-
 Date of Submission: ##/##/####
-
+Assignment: Playback Speed
 '''
+
+### Assignment Directions ###
+''' Some people have a habit of lecturing speaking rather quickly, 
+    and it’d be nice to slow them down, a la YouTube’s 0.75 playback speed, 
+    or even by having them pause between words.
+    In a file called playback.py, implement a program in Python 
+    that prompts the user for input and then outputs that same input, 
+    replacing each space with ... (i.e., three periods).
+    For example, if the user enters "I am a slow speaker",
+    the program should output "I...am...a...slow...speaker".
+
+    Hints
+    Recall that input returns a str, per: docs.python.org/3/library/functions.html#input.
+    Recall that a str comes with quite a few methods, 
+    per: docs.python.org/3/library/stdtypes.html#string-methods.
+
+    You can use the str.split method to split a string into a list of substrings.
+    You can use the str.replace method to replace all occurrences of a substring
+    with another substring.
+    You can use the str.join method to join a list of strings into a single string.
+    You can use the str.strip method to remove leading and trailing whitespace from a string.'''
+
+### Get User Input and Store in Variables
+
+### Trim Whitespace
+
+### Replace Spaces with "..."
+
+### Print off Variable

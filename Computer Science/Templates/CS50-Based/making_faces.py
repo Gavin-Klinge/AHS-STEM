ec28c8a63127b3171dfdcb8f33483cf328729201
Gavin Klinge <78894884+Gavin-Klinge@users.noreply.github.com>
2025-04-10 12:34:51 -0500

    Create making_faces.py

---
+++ Computer Science/Templates/CS50-Based/making_faces.py
@@ -0,0 +1,33 @@
+'''
+Name: Last, First
+Class Block: #suffix Block (*example* 3rd Block)
+Date of Creation: ##/##/####
+Date of Submission: ##/##/####
+Assignment: Making Faces
+'''
+
+"""
+Assignment Directions:
+Before there were emoji, there were emoticons. In this assignment, you'll write a program that:
+- Implements a function named convert that:
+  - Accepts a string as input.
+  - Replaces any occurrence of ":)" with "🙂" (a slightly smiling face).
+  - Replaces any occurrence of ":(" with "🙁" (a slightly frowning face).
+  - Returns the updated string.
+- Implements a function named main that:
+  - Prompts the user for input.
+  - Calls the convert function on the input.
+  - Prints the result.
+- Calls the main function at the bottom of the file.
+Hints:
+- The input() function returns a string.
+- You may use string methods such as .replace() to perform the substitutions.
+- Emojis can be used like any character enclosed in quotes, for example "🙂".
+"""
+
+### Get User Input and Store in Variables AND Trim Whitespace 
+
+### Replace ":)" with "🙂" and ":(" with "🙁" in the string
+
+### Print the updated string
+

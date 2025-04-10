79e71437bc4740e24712b3bd5eb5f2330c29e361
Gavin Klinge <78894884+Gavin-Klinge@users.noreply.github.com>
2025-04-10 12:16:12 -0500

    Update indoor_voice.py

--- Computer Science/Templates/CS50-Based/indoor_voice.py
+++ Computer Science/Templates/CS50-Based/indoor_voice.py
@@ -1,1 +1,15 @@
+'''
+Name: Last, First
+Class Block: #suffix Block (*example* 3rd Block)
+Date of Creation: ##/##/####
+Date of Submission: ##/##/####
+Assignment: Assignment Name
+'''
 
+### Get User Input and Store in Variables
+
+### Trim Whitespace
+
+### Convert String to Lowercase
+
+### Print off Variable
